e82fd64006638201803bfb2b37a16743c99d18a8
renbin.guo <949409706@qq.com>
2017-08-05 16:02:59 +0800

    add file 10-1.py 10-2.py

---
+++ 10-1-grb-切片.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#================================================================
+#  Copyright (C) 2017 free for learn
+#   
+#     file:	09-1-grb-ÇÐÆ¬.py
+#    brief£º
+#  history:	2017-08-05  renbin.guo created
+#     note:	
+#    usage:	
+#
+#================================================================
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print('L[0:3] =', L[0:3])
+print('L[:3] =', L[:3])
+print('L[1:3] =', L[1:3])
+print('L[-2:] =', L[-2:])
+
+R = list(range(100))
+print('R[:10] =', R[:10])
+print('R[-10:] =', R[-10:])
+print('R[10:20] =', R[10:20])
+print('R[:10:2] =', R[:10:2])
+print('R[::5] =', R[::5])
+
+
