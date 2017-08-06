419f964c038e8fb574da5c66860f23bc99505daa
renbin.guo <949409706@qq.com>
2017-08-06 08:10:26 +0800

    add comment 17-sorted.py

--- 17-sorted.py
+++ 17-sorted.py
@@ -13,6 +13,9 @@ print(sorted(['Bod','as','sf','afa','zoasd'], key = str.lower))
 # 要进行反向排序，不必改动key函数，可以传入第三个参数reverse=True：
 print(sorted(['Bod','as','sf','afa','zoasd'], key = str.lower, reverse = True))
 
+
+
+## 练习
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_name(t):
 	return t[0].lower()
