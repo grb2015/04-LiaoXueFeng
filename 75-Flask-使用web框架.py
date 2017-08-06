e64a524c0eb67047d2998f17158147faed6768b9
grb <473602668@qq.com>
2017-08-05 19:27:42 -0700

    rename and comment 75.py

--- 75-Flask-使用web框架.py
+++ 75-Flask-使用web框架.py
@@ -1,3 +1,21 @@
+## 之前用WSGI写的http服务器可以正常工作，但是有个问题，它如何处理不同的url，比如如何处理100个不同的URL ?
+## 
+# 一个最简单的想法是从environ变量里取出HTTP请求的信息，然后逐个判断：
+
+# def application(environ, start_response):
+#    method = environ['REQUEST_METHOD']
+#    path = environ['PATH_INFO']
+#    if method=='GET' and path=='/':            # 处理第一种url:  GET / 
+#        return handle_home(environ, start_response)
+#    if method=='POST' and path='/signin':      # 处理第二种url:   POST  /signin    
+#        return handle_signin(environ, start_response)
+#       ...
+#                                               # 处理第100中url：
+
+
+# 这样写的代码肯定无法维护了
+# 我们想用一个函数处理一个URL，至于URL到函数的映射，就交给Web来做。(所以web框架可以帮我们实现 从url到函数的映射)
+
 '''
 使用Web框架-Flask
 
@@ -23,10 +41,12 @@ from flask import request
 
 app = Flask(__name__)
 
+## 处理第一种url    GET  /      or  POST  /
 @app.route('/',methods=['GET','POST'])
 def home():
     return '<h1>Home</h1>'
 
+## 处理第2种url    GET  /signin
 @app.route('/signin',methods=['GET'])
 def signin_form():
     return '''<form action='/signin' method='post'>
@@ -34,6 +54,7 @@ def signin_form():
               <p><input name='password' type='password'></p>
               <p><button type='submit'>Sign In</button></p>'''
 
+## 处理第2种url    POST  /signin
 @app.route('/signin',methods=['POST'])
 def signin():
     # 需要从request对象读取表单内容：
@@ -41,9 +62,11 @@ def signin():
         return '<h3>Hello,admin..</h3>'
     return '<h3>bad username or password</h3>'
 
+
+# 开始执行程序
 if __name__ == '__main__':
     print(app)
-    app.run()
+    app.run()       ##  运行python，Flask自带的Server在端口5000上监听.我们写的只是处理http的逻辑
 
 '''
 运行python，Flask自带的Server在端口5000上监听：
